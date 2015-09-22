982c42f433d584b2aef81661881c24d9b24514a7
Proskurina <yproskurina@yahoo.com>
2015-09-22 16:27:35 +0100

    Can filter by tag

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -2,13 +2,26 @@ require_relative '../../app/data_mapper_setup'
 
 feature 'Viewing links' do
 
+  before(:each) do
+    Link.create(url: 'http://www.makersacademy.com',
+                title: 'Makers Academy',
+                tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'http://www.google.com',
+                title: 'Google',
+                tags: [Tag.first_or_create(name: 'search')])
+    Link.create(url: 'http://www.zombo.com',
+                title: 'This is Zombocom',
+                tags: [Tag.first_or_create(name: 'bubbles')])
+    Link.create(url: 'http://www.bubble-bobble.com',
+                title: 'Bubble Bobble',
+                tags: [Tag.first_or_create(name: 'bubbles')])
+  end
+
+
   scenario 'I can see existing links on the link page' do
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
-
     visit '/links'
-
     expect(page.status_code).to eq 200
-
     within 'ul#links' do
       expect(page).to have_content('Makers Academy')
     end
@@ -19,6 +32,16 @@ feature 'Viewing links' do
     visit '/links'
     click_button('Create link')
     expect(current_path).to eq '/links/new'
+  end
+
+  scenario 'I can filter links by tag' do
+    visit '/tags/bubbles'
+    within 'ul#links' do
+      expect(page).not_to have_content('Makers Academy')
+      expect(page).not_to have_content('Code.org')
+      expect(page).to have_content('This is Zombocom')
+      expect(page).to have_content('Bubble Bobble')
+    end
   end
 
 end
