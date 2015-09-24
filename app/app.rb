6c8db7e13fb8451c13dbb296cf9ce1920622e18c
Emily Worrall <worrall.emily@gmail.com>
2015-09-24 12:08:58 +0100

    Flash errors all working

--- app/app.rb
+++ app/app.rb
@@ -45,22 +45,15 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    if params[:email] == '' || params[:email].nil?
-      flash.now[:notice] = 'Must enter an email address'
-      erb :'users/new'
+    @user = User.new(email: params[:email],
+                     password: params[:password],
+                     password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/')
     else
-      @user = User.new(email: params[:email],
-                         password: params[:password],
-                         password_confirmation: params[:password_confirmation])
-      if @user.save
-        session[:user_id] = @user.id
-        redirect to('/')
-      else
-        # flash.now[:notice] = "Password and confirmation password do not match"
-        # erb :'users/new'
-        flash.now[:errors] = @user.errors
-        erb :'users/new'
-      end
+      flash.now[:errors] = @user.errors
+      erb :'users/new'
     end
   end
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -2,32 +2,31 @@ require_relative '../../app/data_mapper_setup'
 
 feature 'User sign up' do
 
-  # scenario 'I can sign up as a new user' do
-  #   user = create :user
-  #   expect { sign_up_as(user) }.to change(User, :count).by(1)
-  #   expect(page).to have_content("Welcome, #{user.email}")
-  #   expect(User.first.email).to eq('alice@example.com')
-  # end
-  #
-  # scenario 'requires a matching confirmation password' do
-  #   user = create :user
-  #   user.password_confirmation = 'wrong'
-  #   expect {sign_up_as(user)}.not_to change(User, :count)
-  #   expect(current_path).to eq('/users')
-  #   expect(page).to have_content 'Password and confirmation password do not match'
-  # end
-  #
-  # scenario 'has not entered an email address' do
-  #   user = create :user
-  #   user.email = ''
-  #   expect { sign_up_as(user) }.not_to change(User, :count)
-  #   expect(current_path).to eq('/users')
-  #   expect(page).to have_content 'Must enter an email address'
-  # end
+  scenario 'I can sign up as a new user' do
+    user = create :user
+    user.email = 'bob@example.com'
+    expect { sign_up_as(user) }.to change(User, :count).by(1)
+    expect(page).to have_content("Welcome, bob@example.com")
+  end
+
+  scenario 'requires a matching confirmation password' do
+    user = create :user
+    user.password_confirmation = 'wrong'
+    expect {sign_up_as(user)}.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content 'Password does not match the confirmation'
+  end
+
+  scenario 'has not entered an email address' do
+    user = create :user
+    user.email = ''
+    expect { sign_up_as(user) }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content 'Email must not be blank'
+  end
 
   scenario 'I cannot sign up with existing email' do
     user = create :user
-    sign_up_as(user)
     user.password = 'wrong'
     expect{sign_up_as(user)}.to change(User, :count).by(0)
     expect(page).to have_content('Email is already taken')
